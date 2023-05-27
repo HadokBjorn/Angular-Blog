4f64153e442d5706bc7126d1335ce754298975e0
Esaú Bandeira <esaubandeira.dev@gmail.com>
2023-05-27 14:18:34 -0500

    transform when hover

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuBarComponent implements OnInit {
   isRender:string = "menu-disabled";
+  logoImage: string = "https://cdn.dribbble.com/users/297873/screenshots/10694373/media/dc1ee39bd96cddf2527d0817822d55f0.jpg?compress=1&resize=400x300&vertical=top"
   constructor() { }
 
   ngOnInit(): void {
